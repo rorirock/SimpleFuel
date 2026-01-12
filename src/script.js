faf2090ac4b16c0d8d2e37d655fe76fd8a0ebdbf
RodrigoJimenez <rodrigojimenez82@gmail.com>
2026-01-12 15:27:03 -0500

    first commit of code

---
+++ src/script.js
@@ -0,0 +1,13 @@
+ const menuToggle = document.querySelector('.menu-toggle');
+        const navLinks = document.querySelector('.nav-links');
+
+        menuToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+
+        // Cerrar menú al hacer clic en un enlace
+        document.querySelectorAll('.nav-links a').forEach(link => {
+            link.addEventListener('click', () => {
+                navLinks.classList.remove('active');
+            });
+        });
